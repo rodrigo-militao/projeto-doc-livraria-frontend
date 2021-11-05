a291e5af474b8f6ddbc80d2937b74846b4f7db6e
rodrigo-militao <militaodorigo9@gmail.com>
2021-11-05 15:48:21 -0300

    fist commit

---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div>
+            Home, aqui vou pegar todos os articles.
+        </div>
+    )
+}
---
+++ src/pages/NewArticle.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NewArticle() {
+    return (
+        <div>
+            Aqui iremos criar um novo artigo
+        </div>
+    )
+}
